50b9db198622abaa35a433051fa29899f8bfbb4f
Christerpher Hunter <djhunter67@gmail.com>
2022-02-04 01:30:43 -0500

    pytest.ini is correctly set up

--- capstone/reservations/tests.py
+++ capstone/reservations/tests.py
@@ -1,8 +1,5 @@
-from django.test import TestCase
-
 import pytest
 
-from reservations.views import reservations
 from .models import Reservation
 from django.utils import timezone
 # Create your tests here.
@@ -11,13 +8,13 @@ from django.utils import timezone
 @pytest.mark.django_db
 def test_reservation_create():
     reservation = Reservation.objects.create(
-        electricity_used = 3.25,
-        hours_used = 5.4,
-        auto_bay_id = 2,
-        diy_user_id = 7,
-        date_reserved = timezone.now(),
-        reservation_date=timezone.activate("America/New_York"),
-        time_limit=4,
+        electricity_used=3.25,
+        hours_used=5.4,
+        # auto_bay_id=2,
+        # diy_user_id="user11",
+        date_reserved=timezone.now(),
+        # reservation_date=timezone.activate("America/New_York"),
+        # time_limit=4,
     )
 
     assert reservation.time_limit == 4
