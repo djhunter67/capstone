aa67afba2bb083e54228bddef1c47bcf637bc32d
Christerpher Hunter <djhunter67@gmail.com>
2022-02-07 20:09:39 -0500

    attempting to solve django db issue

--- capstone/reservations/models.py
+++ capstone/reservations/models.py
@@ -6,6 +6,7 @@ from hunter_diy_garage.models import AutoBay
 
 # Create your models here.
 
+
 class Reservation(models.Model):
 
     electricity_used = models.DecimalField(
@@ -13,11 +14,11 @@ class Reservation(models.Model):
     hours_used = models.DecimalField(
         default=0.0, max_digits=5, decimal_places=2)
     # auto_bay_id = models.ForeignKey(
-        # AutoBay, on_delete=models.SET_NULL, null=True, related_name="reservations", to_field="name", unique=True)
-    auto_bay_id = models.OneToOneField(AutoBay, on_delete=models.SET_NULL, null=True, related_name="reservations")        
+    # AutoBay, on_delete=models.SET_NULL, null=True, related_name="reservations", to_field="name", unique=True)
+    auto_bay_id = models.OneToOneField(
+        AutoBay, on_delete=models.SET_NULL, null=True, related_name="reservations")
     diy_user_id = models.ForeignKey(
         DIYUsers, on_delete=models.CASCADE, related_name="reservations")
     date_reserved = models.DateTimeField(auto_now_add=True)
     reservation_date = models.DateTimeField()
     time_limit = models.IntegerField(default=0)
-
--- capstone/reservations/tests.py
+++ capstone/reservations/tests.py
@@ -1,4 +1,7 @@
 import pytest
+from diy_users.models import DIYUsers
+
+from hunter_diy_garage.models import AutoBay
 
 from .models import Reservation
 from django.utils import timezone
@@ -7,15 +10,21 @@ from django.utils import timezone
 
 @pytest.mark.django_db
 def test_reservation_create():
+
+    reservation = Reservation()
+    # reservation.save(force_insert=False, force_update=False)
+    auto_bay_id=AutoBay()
+    diy_user_id=DIYUsers()
+
     reservation = Reservation.objects.create(
         electricity_used=3.25,
         hours_used=5.4,
-        # auto_bay_id=2,
-        # diy_user_id="user11",
+        auto_bay_id=2,
+        diy_user_id="user11",
         date_reserved=timezone.now(),
-        # reservation_date=timezone.activate("America/New_York"),
-        # time_limit=4,
+        reservation_date=timezone.activate("America/New_York"),
+        time_limit=4,
     )
 
     assert reservation.time_limit == 4
-    assert reservation.auto_bay_id == 2
+    assert reservation.electricity_used == 3.25
